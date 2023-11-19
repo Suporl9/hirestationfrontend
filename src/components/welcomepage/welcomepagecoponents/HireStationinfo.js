6063142fbb50b8c0e39205e985c6af0845d4fb64
Suman <sumanpok9@gmail.com>
2023-11-19 23:56:20 +0545

    fix: edit descricption

--- src/components/welcomepage/welcomepagecoponents/HireStationinfo.js
+++ src/components/welcomepage/welcomepagecoponents/HireStationinfo.js
@@ -1,21 +1,21 @@
-import React from "react";
-import "./css/welcomestyles.css";
+import React from 'react';
+import './css/welcomestyles.css';
 export default function HireStationinfo() {
   return (
-    <div className="descriptionsection">
-      <div className="h1p1whatissection">
-        <h1 className="h1whatis">
-          What&nbsp;is&nbsp;<div className="h1whatisred">Hire</div>Station?
+    <div className='descriptionsection'>
+      <div className='h1p1whatissection'>
+        <h1 className='h1whatis'>
+          What&nbsp;is&nbsp;<div className='h1whatisred'>Hire</div>Station?
         </h1>
-        <p className="p1whatis">
-          Exercitation sint ipsum velit velit pariatur commodo quis culpa
-          consectetur. Cupidatat nostrud consectetur eu minim. Sint deserunt
-          irure id consequat dolore mollit. Sit nulla ullamco do ea nostrud id
-          labore minim ea veniam qui. In cupidatat sunt dolore excepteur tempor
-          proident.Culpa laborum mollit et ipsum reprehenderit occaecat eu
-          adipisicing excepteur. Eiusmod nostrud amet adipisicing ut ut fugiat
-          eiusmod consequat elit Lorem aliquip. Minim nisi deserunt proident
-          amet sit proident commodo irure veniam non sint.
+        <p className='p1whatis'>
+          ‘Hire Station’ is a web-based service listing website for freelancers.
+          It provides a system where freelancers can post their gigs or hire
+          someone to do their gigs while paying a certain payment amount. Our
+          backend mainly focuses on automation of different gigs or services
+          retrieval, Wishlist, Orders and payment system for each user logging
+          in our website. This project is an attempt to provide the advantages
+          of online freelancing to people looking for a place to sell or buy
+          services.
         </p>
       </div>
     </div>
